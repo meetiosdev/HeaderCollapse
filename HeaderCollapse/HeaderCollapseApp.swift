6fc0aea84bc0f37251c2cca05a7c5b06a4449043
Swarajmeet Singh <swarajmeet.crebos@gmail.com>
2025-05-24 15:04:37 +0530

    Initial Commit

---
+++ HeaderCollapse/HeaderCollapseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderCollapseApp.swift
+//  HeaderCollapse
+//
+//  Created by Swarajmeet Singh on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HeaderCollapseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
